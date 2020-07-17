5238a534ace99f6ca3657effc97412e3e399b8e1
Elton Lika <eltonlika1@gmail.com>
2020-07-17 15:53:13 +0200

    simplify code

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/eltonlika/posta-tracking/cli"
+	"github.com/eltonlika/posta-tracking/tracker"
 )
 
 func main() {
-	os.Exit(cli.Run())
+	if len(os.Args) < 2 {
+		fmt.Println("Error: No tracking number given")
+		fmt.Printf("Usage: %s <tracking number>\n", os.Args[0])
+		return
+	}
+
+	events, err := tracker.NewTracker().Track(os.Args[1])
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return
+	}
+
+	if events.Len() > 0 {
+		fmt.Println(events)
+	}
 }
--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,25 +1,23 @@
 package tracker
 
 import (
-	"errors"
+	"fmt"
 	"sort"
 	"time"
 
 	q "github.com/PuerkitoBio/goquery"
 	"github.com/headzoo/surf/agent"
 	"github.com/headzoo/surf/browser"
+	"github.com/ryanuber/columnize"
 	"gopkg.in/headzoo/surf.v1"
 )
 
-const (
-	// DefaultServiceURL url of tracking service page
-	DefaultServiceURL = "https://gjurmo.postashqiptare.al/tracking.aspx"
-	// DefaultTimeout is 8 seconds of waiting for a tracking request
-	DefaultTimeout = time.Second * 8
-)
+// ServiceURL url of tracking service page
+const ServiceURL = "https://gjurmo.postashqiptare.al/tracking.aspx"
 
 // Event holds tracking event information
 type Event struct {
+	Num            uint
 	Date           time.Time
 	TrackingNumber string
 	Description    string
@@ -30,24 +28,47 @@ type Event struct {
 // Events array type
 type Events []Event
 
+// Sort interface implementations
 func (events Events) Len() int           { return len(events) }
 func (events Events) Less(i, j int) bool { return events[i].Date.Before(events[j].Date) }
 func (events Events) Swap(i, j int)      { events[i], events[j] = events[j], events[i] }
 
-// Sort events by specified direction
-func (events Events) Sort(descending bool) {
-	if descending {
-		sort.Stable(sort.Reverse(events))
-	} else {
-		sort.Stable(events)
+// Event fmt interface implementation
+func (e Event) String() string {
+	return fmt.Sprintf("%d|%s|%s|%s|%s|%s",
+		e.Num,
+		e.TrackingNumber,
+		e.Date.Format("2006-01-02 15:04 PM"),
+		e.Description,
+		e.Location,
+		e.Destination)
+}
+
+// Events fmt interface implementation
+func (events Events) String() string {
+	if events.Len() == 0 {
+		return ""
 	}
+
+	table := make([]string, events.Len()+1)
+
+	table[0] = "#|Kodi|Data|Ngjarja|Zyra|Destinacioni"
+
+	for i, e := range events {
+		table[i+1] = e.String()
+	}
+
+	config := columnize.DefaultConfig()
+	config.Delim = "|"
+	config.Glue = "  "
+	config.Prefix = ""
+	config.Empty = ""
+	config.NoTrim = false
+	return columnize.Format(table, config)
 }
 
 // Tracker struct mantains tracker instance & configuration
 type Tracker struct {
-	ServiceURL           string
-	SortEventsDescending bool
-
 	browser *browser.Browser
 }
 
@@ -56,97 +77,81 @@ func NewTracker() Tracker {
 	bow := surf.NewBrowser()
 	bow.SetAttribute(browser.FollowRedirects, true)
 	bow.SetUserAgent(agent.Chrome())
-	bow.SetTimeout(DefaultTimeout)
-
-	return Tracker{
-		ServiceURL:           DefaultServiceURL,
-		SortEventsDescending: false,
-		browser:              bow,
-	}
+	return Tracker{browser: bow}
 }
 
 // Track returns tracking events for given tracking number
 func (tracker Tracker) Track(trackingNumber string) (Events, error) {
-	events, err := tracker.findTrackingEvents(trackingNumber)
+	if err := tracker.browser.Open(ServiceURL); err != nil {
+		return nil, err
+	}
+
+	form, err := tracker.browser.Form("#form1")
 	if err != nil {
 		return nil, err
 	}
 
-	events.Sort(tracker.SortEventsDescending)
-	return events, err
-}
-
-// SetRequestTimeout set time to wait for response from tracking service
-func (tracker Tracker) SetRequestTimeout(seconds uint) {
-	tracker.browser.SetTimeout(time.Second * time.Duration(seconds))
-}
-
-func (tracker Tracker) findTrackingEvents(trackingNumber string) (Events, error) {
-	browser := tracker.browser
-
-	if err := browser.Open(tracker.ServiceURL); err != nil {
+	form.Input("txt_barcode", trackingNumber)
+	form.Input("hBarCodes", trackingNumber)
+	if err = form.Submit(); err != nil {
 		return nil, err
 	}
 
-	fm, err := browser.Form("#form1")
+	events, err := parseEvents(trackingTableValues(tracker.browser.Dom()))
 	if err != nil {
 		return nil, err
 	}
 
-	fm.Input("txt_barcode", trackingNumber)
-	fm.Input("hBarCodes", trackingNumber)
-	if err = fm.Submit(); err != nil {
-		return nil, err
-	}
+	sort.Stable(events)
 
-	events, err := extractEvents(browser.Dom())
-	if err != nil {
-		return nil, err
-	}
-
-	// set each event's tracking number field
-	for i := range events {
+	// set each event's sequence num and tracking number
+	for i := 0; i < events.Len(); i++ {
+		events[i].Num = uint(i + 1)
 		events[i].TrackingNumber = trackingNumber
 	}
 
 	return events, nil
 }
 
-func extractText(_ int, s *q.Selection) string { return s.Text() }
-
-func extractEvent(s *q.Selection) (Event, error) {
-	event := Event{}
-
-	rowValues := s.ChildrenFiltered("td").Map(extractText)
-	if len(rowValues) < 4 {
-		return event, errors.New("Row has less values than expected")
-	}
-
-	eventDate, err := time.Parse("02-01-2006 15:04 PM", rowValues[0])
-	if err != nil {
-		return event, err
-	}
-
-	event.Date = eventDate
-	event.Description = rowValues[1]
-	event.Location = rowValues[2]
-	event.Destination = rowValues[3]
-
-	return event, nil
+func cellValue(_ int, s *q.Selection) string {
+	return s.Text()
 }
 
-func isTableDataRow(_ int, tr *q.Selection) bool { return tr.ChildrenFiltered("td").Length() > 0 }
+func tdRow(_ int, s *q.Selection) bool {
+	return s.ChildrenFiltered("td").Length() > 0
+}
 
-func extractEvents(dom *q.Selection) (Events, error) {
-	// get only rows with data cells, exclude header row
-	tableDataRows := dom.Find("table#gvTraking tr").FilterFunction(isTableDataRow)
-	events := make(Events, tableDataRows.Length())
+func trackingTableValues(dom *q.Selection) [][]string {
+	rows := dom.Find("table#gvTraking tr").FilterFunction(tdRow)
 
-	var err error
-	tableDataRows.EachWithBreak(func(i int, tr *q.Selection) bool {
-		events[i], err = extractEvent(tr)
-		return err == nil // if has error then stop iteration
+	values := make([][]string, rows.Length())
+
+	rows.Each(func(i int, s *q.Selection) {
+		values[i] = s.ChildrenFiltered("td").Map(cellValue)
 	})
 
-	return events, err
+	return values
+}
+
+func parseEvents(values [][]string) (Events, error) {
+	events := make(Events, len(values))
+
+	for i, row := range values {
+		if len(row) < 4 {
+			return nil, fmt.Errorf("Row %v has less values than expected", row)
+		}
+
+		date, err := time.Parse("02-01-2006 15:04 PM", row[0])
+		if err != nil {
+			return nil, err
+		}
+
+		events[i] = Event{
+			Date:        date,
+			Description: row[1],
+			Location:    row[2],
+			Destination: row[3]}
+	}
+
+	return events, nil
 }
